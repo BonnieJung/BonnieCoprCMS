7c8c5ac1f5f6d37ed99a861ccb4008ba6f5441d2
Bonnie <bohye9912@gmail.com>
2017-11-19 22:04:33 -0500

    using session for message, validation for subject creation input, new page

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,33 +1,29 @@
-<?php require_once("../includes/db_connection.php");?>
-<?php require_once("../includes/functions.php");?>
-<?php include("../includes/layouts/header.php");?>
-<?php
- if(isset($_GET["subject"])){
-	 $selected_subject_id = $_GET["subject"];
-	 $selected_page_id = null;
- } else if(isset($_GET["page"])){
-	 $selected_subject_id = null;
-	 $selected_page_id = $_GET["page"];
- } else {
-	$selected_subject_id = null;
-	$selected_page_id = null;
- }
- 
-?>
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include("../includes/layouts/header.php"); ?>
+<?php find_selected_page(); ?>
 
 <div id="main">
-	<div id="navigation">
-		<?php echo navigation($selected_subject_id, $selected_page_id);?>
-	</div>
-	<div id="page">
-		<h2>Manage Content</h2>
-		<?php echo $selected_subject_id; ?>
-		<?php echo $selected_page_id; ?>
-		
-	</div>
+  <div id="navigation">
+		<?php echo navigation($current_subject, $current_page); ?>
+		<br />
+		<a href="new_subject.php">+ Add a subject</a>
+  </div>
+  <div id="page">
+		<?php echo message(); ?>
+		<?php if ($current_subject) { ?>
+	    <h2>Manage Subject</h2>
+			Menu name: <?php echo $current_subject["menu_name"]; ?><br />
+			
+		<?php } elseif ($current_page) { ?>
+			<h2>Manage Page</h2>
+			Menu name: <?php echo $current_page["menu_name"]; ?><br />
+			
+		<?php } else { ?>
+			Please select a subject or a page.
+		<?php }?>
+  </div>
 </div>
 
-		
-<?php include("../includes/layouts/footer.php");?>
-
-		
+<?php include("../includes/layouts/footer.php"); ?>
